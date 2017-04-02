092eae24384851681307e46f4d85b20f019f57d5
FiveMP <developer@five-multiplayer.net>
2017-04-02 18:53:04 +0200

    Update

--- api.cpp
+++ api.cpp
@@ -6,45 +6,58 @@
 // Math
 #include "sdk/CVector3.h"
 #include "sdk/CVector4.h"
+#include "sdk/Structs.h"
 
 // API Function Imports
 #include "sdk/APIServer.h"
-#include "sdk/APINpc.h"
 #include "sdk/APIWorld.h"
 #include "sdk/APIEntity.h"
 #include "sdk/APIVehicle.h"
 #include "sdk/APIVisual.h"
 #include "sdk/APIPlayer.h"
 #include "sdk/APIObject.h"
+#include "sdk/APICheckpoint.h"
 
 extern "C" DLL_PUBLIC bool API_Initialize(void) {
 	// When Plugin gets loaded
-	API::Server::PrintMessage("Initialized");
+	API::Server::PrintMessage(L"Initialized");
 	return true;
 }
 
 extern "C" DLL_PUBLIC bool API_Close(void) {
 	// When plugin gets unloaded
-	API::Server::PrintMessage("Closed");
+	API::Server::PrintMessage(L"Closed");
 	return true;
 }
 
 extern "C" DLL_PUBLIC bool API_OnTick(void) {
 	// Every server tick this gets called
-	API::Server::PrintMessage("Tick");
+	API::Server::PrintMessage(L"Tick");
 	return true;
 }
 
 extern "C" DLL_PUBLIC bool API_OnPlayerConnecting(const char *guid)
 {
 	// When a player connects (still loading everything from the server)	
-	API::Server::PrintMessage("Connecting");
+	API::Server::PrintMessage(L"Connecting");
 	return true;
 }
 
 extern "C" DLL_PUBLIC bool API_OnPlayerConnected(int player)
 {
 	// When the player is successfully connected (loaded in, but not spawned yet)
-	API::Server::PrintMessage("Connected");
+	API::Server::PrintMessage(L"Connected");
 	return true;
+}
+
+// When a entity enters a checkpoint (only players right now)
+extern "C" DLL_PUBLIC void API_OnEntityEnterCheckpoint(int checkpoint, int entity)
+{
+	API::Server::PrintMessage(L"OnEntityEnterCheckpoint");
+}
+
+// When a entity exits a checkpoint (only players right now)
+extern "C" DLL_PUBLIC void API_OnEntityExitCheckpoint(int checkpoint, int entity)
+{
+	API::Server::PrintMessage(L"OnEntityExitCheckpoint");
 }
--- sdk/APIEntity.h
+++ sdk/APIEntity.h
@@ -8,7 +8,7 @@ extern "C" {
 		class Entity
 		{
 		public:
-			DLL_PUBLIC_I struct PedComponent
+			/*DLL_PUBLIC_I struct PedComponent
 			{
 				int drawableid = -1;
 				int textureid = -1;
@@ -46,20 +46,20 @@ extern "C" {
 			DLL_PUBLIC_I struct PedFeature
 			{
 				float scale = 0.0f;
-			};
+			};*/
 
 			/// <summary>
 			/// Destroys/Removes a entity from the server
 			/// </summary>
 			/// <param name="entity">The entity you wish to destroy.</param>
-			DLL_PUBLIC_I static void Destroy(int entity);
+			DLL_PUBLIC_I static void Destroy(const int entity);
 
 			/// <summary>
 			/// Gets the position of the entity.
 			/// </summary>
 			/// <param name="entity">The entity you wish to get the position off.</param>
 			/// <returns name="position">The CVector3 position of the entity</returns>
-			DLL_PUBLIC_I static const CVector3 GetPosition(int entity);
+			DLL_PUBLIC_I static const CVector3 GetPosition(const int entity);
 
 			/// <summary>
 			/// Sets the position of the entity.
@@ -67,29 +67,14 @@ extern "C" {
 			/// <param name="entity">The entity you wish to set the position off.</param>
 			/// <param name="position">The position you wish to set the entity at.</param>
 			/// <returns></returns>
-			DLL_PUBLIC_I static void SetPosition(int entity, CVector3 position);
-
-			/// <summary>
-			/// Gets the quaternion of the entity.
-			/// </summary>
-			/// <param name="entity">The entity you wish to get the quaternion off.</param>
-			/// <returns name="quaternion">The CVector4 'quaternion' of the entity</returns>
-			DLL_PUBLIC_I static const CVector4 GetQuaternion(int entity);
-
-			/// <summary>
-			/// Sets the quaternion of the entity.
-			/// </summary>
-			/// <param name="entity">The entity you wish to set the quaternion off.</param>
-			/// <param name="quaternion">The quaternion you wish to set the entity at.</param>
-			/// <returns></returns>
-			DLL_PUBLIC_I static void SetQuaternion(int entity, CVector4 quaternion);
+			DLL_PUBLIC_I static void SetPosition(const int entity, const CVector3 position);
 
 			/// <summary>
 			/// Gets the rotation of the entity.
 			/// </summary>
 			/// <param name="entity">The entity you wish to get the rotation off.</param>
 			/// <returns name="rotation">The CVector3 rotation of the entity</returns>
-			DLL_PUBLIC_I CVector3 GetRotation(int entity);
+			DLL_PUBLIC_I CVector3 GetRotation(const int entity);
 
 			/// <summary>
 			/// Sets the rotation of the entity.
@@ -97,7 +82,7 @@ extern "C" {
 			/// <param name="entity">The entity you wish to set the rotation off.</param>
 			/// <param name="rotation">The rotation you wish to set they entity at.</param>
 			/// <returns></returns>
-			DLL_PUBLIC_I void SetRotation(int entity, CVector3 rotation);
+			DLL_PUBLIC_I void SetRotation(const int entity, const CVector3 rotation);
 
 			/// <summary>
 			/// Gets the Ped Component data of the player or npc entity.
@@ -113,7 +98,7 @@ extern "C" {
 			/// <param name="entity">The entity of the player or npc you wish to set their model Components.</param>
 			/// <param name="componentid">The componentid of the component you wish to set.</param>
 			/// <param name="component">The component structure of the component you wish to set.</param>
-			DLL_PUBLIC_I static void SetPedComponent(const int entity, const int componentid, PedComponent component);
+			DLL_PUBLIC_I static void SetPedComponent(const int entity, const int componentid, const PedComponent component);
 
 			/// <summary>
 			/// Gets the head blend data of the player or npc entity.
@@ -127,7 +112,7 @@ extern "C" {
 			/// </summary>
 			/// <param name="entity">The entity of the player or npc you wish to set their model headblend.</param>
 			/// <param name="headblend">The headblend data.</param>
-			DLL_PUBLIC_I static void SetPedHeadBlend(const int entity, PedHeadBlend headblend);
+			DLL_PUBLIC_I static void SetPedHeadBlend(const int entity, const PedHeadBlend headblend);
 
 			/// <summary>
 			/// Gets the headoverlay data of the player or npc entity.
@@ -143,7 +128,7 @@ extern "C" {
 			/// <param name="entity">The entity of the player or npc you wish to set their model headoverlay data.</param>
 			/// <param name="overlayid">The overlay id you wish to set the data off.</param>
 			/// <param name="overlay">The overlay data.</param>
-			DLL_PUBLIC_I static void SetPedHeadOverlay(const int entity, const int overlayid, PedHeadOverlay overlay);
+			DLL_PUBLIC_I static void SetPedHeadOverlay(const int entity, const int overlayid, const PedHeadOverlay overlay);
 
 			/// <summary>
 			/// Gets the prop data of the player or npc entity.
@@ -159,7 +144,7 @@ extern "C" {
 			/// <param name="entity">The entity of the player or npc you wish to set their model prop data.</param>
 			/// <param name="compotentid">The compotent id you wish to set the data off.</param>
 			/// <param name="compotentid">The prop data.</param>
-			DLL_PUBLIC_I static void SetPedProp(const int entity, const int compotentid, PedProp prop);
+			DLL_PUBLIC_I static void SetPedProp(const int entity, const int compotentid, const PedProp prop);
 
 			/// <summary>
 			/// Gets the face feature data of the player or npc entity.
@@ -175,7 +160,21 @@ extern "C" {
 			/// <param name="entity">The entity of the player or npc you wish to set their model face feature data.</param>
 			/// <param name="index">The index you wish to set the data off (Index can be 0 - 19).</param>
 			/// <param name="scale">The face feature scale. (Scale ranges from -1.0 to 1.0)</param>
-			DLL_PUBLIC_I static void SetPedFaceFeature(const int entity, const int index, float scale);
+			DLL_PUBLIC_I static void SetPedFaceFeature(const int entity, const int index, const float scale);
+
+			/// <summary>
+			/// Gets the view distance of the entity.
+			/// </summary>
+			/// <param name="entity">The entity you want to get the view distance off.</param>
+			/// <returns name="distacne">The view distance.</returns>
+			DLL_PUBLIC static const float GetViewDistance(const int entity);
+
+			/// <summary>
+			/// Sets the view distance of the entity.
+			/// </summary>
+			/// <param name="entity">The entity you want to set the view distance off.</param>
+			/// <param name="distance">The distance.</param>
+			DLL_PUBLIC static void SetViewDistance(const int entity, const float distance);
 		};
 	}
 #ifdef __cplusplus
